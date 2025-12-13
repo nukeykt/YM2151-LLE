6620834d148c6fd17b011740db9d14cdec8dca43
nukeykt <alexeytf2@gmail.com>
2025-12-14 04:40:12 +0900

    fsm, timers

--- fmopm.c
+++ fmopm.c
@@ -77,6 +77,8 @@ void FMOPM_Clock(fmopm_t* chip, int clk)
     if (clk2)
         chip->ic = !ic0;
 
+    int ic = chip->ic;
+
 
     int wr0 = (!chip->input.wr && !chip->input.a0 && !chip->input.cs) || chip->ic;
     int wr1 = !chip->input.wr && chip->input.a0 && !chip->input.cs && !chip->ic;
@@ -111,4 +113,238 @@ void FMOPM_Clock(fmopm_t* chip, int clk)
         chip->write1_l[0] = chip->write1_trig;
         chip->write1_l[2] = chip->write1_l[1];
     }
+
+    int write0_en = chip->write0_l[2];
+    int write1_en = chip->write1_l[2];
+
+    if (!chip->input.cs)
+        chip->data1 = chip->input.data;
+    if (!chip->input.wr)
+        chip->data2 = chip->input.data;
+
+    if (clk1)
+    {
+        chip->fsm_cnt[0] = ic ? 0 : ((chip->fsm_cnt[1] + 1) & 31);
+
+        chip->fsm_out[0] = (chip->fsm_cnt[1] & 24) == 24;
+        chip->fsm_out[1] = (chip->fsm_cnt[1] & 24) == 8;
+        chip->fsm_out[2] = (chip->fsm_cnt[1] & 7) == 3;
+        chip->fsm_out[3] = chip->fsm_cnt[1] == 11;
+        chip->fsm_out[4] = chip->fsm_cnt[1] == 11;
+        chip->fsm_out[5] = chip->fsm_cnt[1] == 28;
+        chip->fsm_out[6] = chip->fsm_cnt[1] == 11;
+
+        chip->fsm_out[7] = (chip->fsm_cnt[1] & 15) == 15;
+        chip->fsm_out[8] = (chip->fsm_cnt[1] & 15) == 15;
+        chip->fsm_out[9] = (chip->fsm_cnt[1] & 15) == 4;
+        chip->fsm_out[10] = chip->fsm_cnt[1] == 4;
+        chip->fsm_out[11] = (chip->fsm_cnt[1] & 15) == 11;
+        chip->fsm_out[12] = (chip->fsm_cnt[1] & 14) == 14;
+        chip->fsm_out[13] = (chip->fsm_cnt[1] & 14) == 4;
+        chip->fsm_out[14] = (chip->fsm_cnt[1] & 12) == 0;
+        chip->fsm_out[15] = (chip->fsm_cnt[1] & 15) == 6;
+        chip->fsm_out[16] = chip->fsm_cnt[1] == 29;
+
+        chip->fsm_sh1_l[0] = (chip->fsm_sh1_l[1] << 1) | chip->fsm_out[1];
+        chip->fsm_sh2_l[0] = (chip->fsm_sh2_l[1] << 1) | chip->fsm_out[0];
+
+        if (chip->fsm_op_rst)
+            chip->fsm_op_cnt[0] = 0;
+        else
+            chip->fsm_op_cnt[0] = (chip->fsm_op_cnt[1] + chip->fsm_op_inc) & 3;
+
+        chip->fsm_sync1[1] = chip->fsm_sync1[0];
+        chip->fsm_cycle0_l = chip->fsm_cycle0;
+        chip->fsm_cycle1_l = chip->fsm_cycle1;
+    }
+    if (clk2)
+    {
+        chip->fsm_cnt[1] = chip->fsm_cnt[0];
+
+        chip->fsm_sh1_l[1] = chip->fsm_sh1_l[0];
+        chip->fsm_sh2_l[1] = chip->fsm_sh2_l[0];
+
+        chip->fsm_op_inc = chip->fsm_out[2];
+        chip->fsm_op_rst = chip->fsm_out[3];
+        chip->fsm_op_cnt[1] = chip->fsm_op_cnt[0];
+        chip->fsm_acc_sync = chip->fsm_out[4];
+
+        chip->o_sh1 = (chip->fsm_sh1_l[1] >> 5) & 1;
+        chip->o_sh2 = (chip->fsm_sh2_l[1] >> 5) & 1;
+
+        chip->fsm_sync1[0] = (chip->fsm_sync1[1] << 1) | chip->fsm_out[5];
+
+        chip->fsm_cycle0 = (chip->fsm_sync1[0] >> 3) & 1;
+        chip->fsm_cycle1 = (chip->fsm_sync1[0] >> 4) & 1;
+    }
+
+    if (clk1)
+    {
+        int cnt = chip->busy_cnt[1] + chip->busy_cnt_en[1];
+        int of = (cnt & 32) != 0 || ic;
+        chip->busy_cnt[0] = ic ? 0 : (cnt & 31);
+        chip->busy_cnt_en[0] = write1_en || (chip->busy_cnt_en[1] && !of);
+    }
+    if (clk2)
+    {
+        chip->busy_cnt[1] = chip->busy_cnt[0];
+        chip->busy_cnt_en[1] = chip->busy_cnt_en[0];
+    }
+
+
+    if (clk1)
+    {
+        if (write0_en)
+        {
+            chip->reg_write_01[0] = chip->data1 == (chip->input.ym2164 ? 9 : 1);
+            chip->reg_write_08[0] = chip->data1 == 8;
+            chip->reg_write_0f[0] = chip->data1 == 0xf;
+            chip->reg_write_10[0] = chip->data1 == 0x10;
+            chip->reg_write_11[0] = chip->data1 == 0x11;
+            chip->reg_write_12[0] = chip->data1 == 0x12;
+            chip->reg_write_14[0] = chip->data1 == 0x14;
+        }
+        else
+        {
+            chip->reg_write_01[0] = chip->reg_write_01[1];
+            chip->reg_write_08[0] = chip->reg_write_08[1];
+            chip->reg_write_0f[0] = chip->reg_write_0f[1];
+            chip->reg_write_10[0] = chip->reg_write_10[1];
+            chip->reg_write_11[0] = chip->reg_write_11[1];
+            chip->reg_write_12[0] = chip->reg_write_12[1];
+            chip->reg_write_14[0] = chip->reg_write_14[1];
+        }
+        
+        if (ic)
+        {
+            chip->reg_test[0] = 0;
+            chip->reg_timer_a[0] = 0;
+            chip->reg_timer_b[0] = 0;
+            chip->reg_timer_a_load[0] = 0;
+            chip->reg_timer_b_load[0] = 0;
+            chip->reg_timer_a_irq[0] = 0;
+            chip->reg_timer_b_irq[0] = 0;
+        }
+        else
+        {
+            if (write1_en && chip->reg_write_01[1])
+                chip->reg_test[0] = data1;
+            else
+                chip->reg_test[0] = chip->reg_test[1];
+            chip->reg_timer_a[0] = chip->reg_timer_a[1];
+            if (write1_en && chip->reg_write_10[1])
+            {
+                chip->reg_timer_a[0] & ~= 0x3fc;
+                chip->reg_timer_a[0] |= chip->data1 << 2;
+            }
+            if (write1_en && chip->reg_write_11[1])
+            {
+                chip->reg_timer_a[0] & ~= 0x3;
+                chip->reg_timer_a[0] |= chip->data1 & 3;
+            }
+            if (write1_en && chip->reg_write_12[1])
+                chip->reg_timer_b[0] = chip->data1;
+            else
+                chip->reg_timer_b[0] = chip->reg_timer_b[1];
+            if (write1_en && chip->reg_write_14[1])
+            {
+                chip->reg_timer_a_load[0] = chip->data1 & 1;
+                chip->reg_timer_b_load[0] = (chip->data1 >> 1) & 1;
+                chip->reg_timer_a_irq[0] = (chip->data1 >> 2) & 1;
+                chip->reg_timer_b_irq[0] = (chip->data1 >> 3) & 1;
+            }
+            else
+            {
+                chip->reg_timer_a_load[0] = chip->reg_timer_a_load[1];
+                chip->reg_timer_b_load[0] = chip->reg_timer_b_load[1];
+                chip->reg_timer_a_irq[0] = chip->reg_timer_a_irq[1];
+                chip->reg_timer_b_irq[0] = chip->reg_timer_b_irq[1];
+            }
+        }
+    }
+    if (clk2)
+    {
+        chip->reg_write_01[1] = chip->reg_write_01[0];
+        chip->reg_write_08[1] = chip->reg_write_08[0];
+        chip->reg_write_0f[1] = chip->reg_write_0f[0];
+        chip->reg_write_10[1] = chip->reg_write_10[0];
+        chip->reg_write_11[1] = chip->reg_write_11[0];
+        chip->reg_write_12[1] = chip->reg_write_12[0];
+        chip->reg_write_14[1] = chip->reg_write_14[0];
+
+        chip->reg_test[1] = chip->reg_test[0];
+        chip->reg_timer_a[1] = chip->reg_timer_a[0];
+        chip->reg_timer_b[1] = chip->reg_timer_b[0];
+        chip->reg_timer_a_load[1] = chip->reg_timer_a_load[0];
+        chip->reg_timer_b_load[1] = chip->reg_timer_b_load[0];
+        chip->reg_timer_a_irq[1] = chip->reg_timer_a_irq[0];
+        chip->reg_timer_b_irq[1] = chip->reg_timer_b_irq[0];
+    }
+
+    if (clk1)
+    {
+        int cnt_a = chip->timer_a_cnt[1] + chip->timer_a_inc;
+        chip->timer_a_of = (cnt_a >> 10) & 1;
+        if (chip->timer_a_load)
+            chip->timer_a_cnt[0] = chip->reg_timer_a[1];
+        else
+            chip->timer_a_cnt[0] = chip->timer_a_en[0] ? cnt_a & 0x3ff : 0;
+        chip->timer_a_en[1] = chip->timer_a_en[0];
+        chip->timer_a_reset[0] = write1_en && chip->reg_write_14[1] && (chip->data1 & 16) != 0;
+        chip->timer_a_status[0] = chip->timer_a_set || (chip->timer_a_status[1] && !chip->timer_a_reset[1]);
+
+        int cnt_b = chip->timer_b_cnt[1] + chip->timer_b_inc;
+        chip->timer_b_of = (cnt_b >> 8) & 1;
+        if (chip->timer_b_load)
+            chip->timer_b_cnt[0] = chip->reg_timer_b[1];
+        else
+            chip->timer_b_cnt[0] = chip->timer_b_en[0] ? cnt_b & 0xff : 0;
+        chip->timer_b_en[1] = chip->timer_b_en[0];
+        chip->timer_b_reset[0] = write1_en && chip->reg_write_14[1] && (chip->data1 & 32) != 0;
+        chip->timer_b_status[0] = chip->timer_b_set || (chip->timer_b_status[1] && !chip->timer_b_reset[1]);
+
+        int subcnt = chip->timer_b_subcnt[1] + chip->fsm_cycle0;
+        int of;
+        if (chip->input.ym2164)
+        {
+            of = (subcnt >> 5) & 1;
+            subcnt &= 31;
+        }
+        else
+        {
+            of = (subcnt >> 4) & 1;
+            subcnt &= 15;
+        }
+        if (ic)
+            chip->timer_b_subcnt[0] = 0;
+        else
+            chip->timer_b_subcnt[0] = subcnt;
+        chip->timer_b_subcnt_of = of;
+    }
+    if (clk2)
+    {
+        int test = (chip->reg_test[0] & 4) != 0;
+        chip->timer_a_cnt[1] = chip->timer_a_cnt[0];
+        chip->timer_a_load = chip->timer_a_of || (!chip->timer_a_en[1] && chip->reg_timer_a_load_l);
+        chip->timer_a_en[0] = chip->reg_timer_a_load_l;
+        chip->timer_a_inc = (chip->reg_timer_a_load_l && chip->fsm_cycle0_l) || test;
+        int rst_a = chip->timer_a_reset[0] || ic;
+        chip->timer_a_reset[1] = rst_a;
+        chip->timer_a_set = chip->timer_a_of && !rst_a && chip->reg_timer_a_irq[1];
+        chip->timer_a_status[1] = chip->timer_a_status[0];
+
+        chip->timer_b_cnt[1] = chip->timer_b_cnt[0];
+        chip->timer_b_load = chip->timer_b_of || (!chip->timer_b_en[1] && chip->reg_timer_b_load[0]);
+        chip->timer_b_en[0] = chip->reg_timer_b_load[0];
+        chip->timer_b_inc = (chip->reg_timer_b_load[0] && chip->timer_b_subcnt_of) || test;
+        int rst_b = chip->timer_b_reset[0] || ic;
+        chip->timer_b_reset[1] = rst_b;
+        chip->timer_b_set = chip->timer_b_of && !rst_b && chip->reg_timer_b_irq[1];
+        chip->timer_b_status[1] = chip->timer_b_status[0];
+
+        chip->timer_b_subcnt[1] = chip->timer_b_subcnt[0];
+    }
+
+    if (chip->fsm_cycle1 && chip->fsm_cycle1_l)
+        chip->reg_timer_a_load_l = chip->reg_timer_a_load[0];
 }
--- fmopm.h
+++ fmopm.h
@@ -2,6 +2,7 @@
 
 
 typedef struct {
+    int ym2164;
     int clk;
     int ic; // neg
     int cs; // neg
@@ -27,7 +28,64 @@ typedef struct {
     int write1_trig;
     int write1_l[3];
 
+    int data1;
+    int data2;
+
+    int fsm_cnt[2];
+    int fsm_out[17];
+    int fsm_sh1_l[2];
+    int fsm_sh2_l[2];
+    int fsm_op_inc;
+    int fsm_op_rst;
+    int fsm_op_cnt[2];
+    int fsm_acc_sync;
+    int fsm_sync1[2];
+    int fsm_cycle0;
+    int fsm_cycle0_l;
+    int fsm_cycle1;
+    int fsm_cycle1_l;
+
+    int reg_write_01[2];
+    int reg_write_08[2];
+    int reg_write_0f[2];
+    int reg_write_10[2];
+    int reg_write_11[2];
+    int reg_write_12[2];
+    int reg_test[2];
+    int reg_timer_a[2];
+    int reg_timer_b[2];
+    int reg_timer_a_load[2];
+    int reg_timer_b_load[2];
+    int reg_timer_a_load_l;
+    int reg_timer_b_load_l;
+    int reg_timer_a_irq[2];
+    int reg_timer_b_irq[2];
+
+    int timer_a_cnt[2];
+    int timer_a_inc;
+    int timer_a_of;
+    int timer_a_en[2];
+    int timer_a_load;
+    int timer_a_reset[2];
+    int timer_a_set;
+    int timer_a_status[2];
+    int timer_b_cnt[2];
+    int timer_b_inc;
+    int timer_b_of;
+    int timer_b_en[2];
+    int timer_b_load;
+    int timer_b_reset[2];
+    int timer_b_set;
+    int timer_b_status[2];
+    int timer_b_subcnt[2];
+    int timer_b_subcnt_of;
+
+    int busy_cnt[2];
+    int busy_cnt_en[2];
+
     int o_sy;
+    int o_sh1;
+    int o_sh2;
 
     int tm_w1;
     int tm_w2;
